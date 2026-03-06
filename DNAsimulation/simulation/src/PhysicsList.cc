492477efa35625886a801438d003b27291355c47
Chiara De Sio <chiara.desio@bristol.ac.uk>
2026-03-06 10:40:05 +0000

    Align Lu177 DNA physics selection with DaRT

--- DNAsimulation/simulation/src/PhysicsList.cc
+++ DNAsimulation/simulation/src/PhysicsList.cc
@@ -26,7 +26,8 @@
 
 #include "PhysicsList.hh"
 #include "G4SystemOfUnits.hh"
-#include "G4EmDNAPhysics_option2.hh"
+#include "G4EmDNAPhysics_option7.hh"
+#include "G4EmAndDNA.hh"
 #include "G4EmDNAChemistry_option3.hh"
 #include "G4PhysicsConstructorRegistry.hh"
 #include "CommandLineParser.hh"
@@ -43,13 +44,22 @@ PhysicsList::PhysicsList()
 {
     SetDefaultCutValue(1.0*nanometer);
     SetVerboseLevel(1);
-    RegisterConstructor("G4EmDNAPhysics_option2");
-  if(CommandLineParser::GetParser()->GetCommandIfActive("-chemOFF")==0)
-{
-    RegisterConstructor("G4EmDNAChemistry_option3");
-}
-    G4ProductionCutsTable::GetProductionCutsTable()->
-    SetEnergyRange(5*eV, 1*GeV);
+
+    if (CommandLineParser::GetParser()->GetCommandIfActive("-PS"))
+    {
+        RegisterConstructor("G4EmDNAPhysics_option7");
+    }
+    else
+    {
+        RegisterConstructor("G4EmAndDNA");
+    }
+
+    if (CommandLineParser::GetParser()->GetCommandIfActive("-chemOFF") == 0)
+    {
+        RegisterConstructor("G4EmDNAChemistry_option3");
+    }
+
+    G4ProductionCutsTable::GetProductionCutsTable()->SetEnergyRange(5*eV, 1*GeV);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -60,14 +70,14 @@ PhysicsList::~PhysicsList()
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 void PhysicsList::ConstructParticle()
 {
-    if(fDNAPhysicsList != nullptr)    
+    if(fDNAPhysicsList != nullptr)
     {
-        fDNAPhysicsList->ConstructParticle(); 
+        fDNAPhysicsList->ConstructParticle();
     }
-    
-    if(fChemistryList_option3 != nullptr) 
+
+    if(fChemistryList_option3 != nullptr)
     {
-        fChemistryList_option3->ConstructParticle(); 
+        fChemistryList_option3->ConstructParticle();
     }
 }
 
@@ -76,48 +86,51 @@ void PhysicsList::ConstructParticle()
 void PhysicsList::ConstructProcess()
 {
     AddTransportation();
-    
-    if(fDNAPhysicsList != nullptr)    
-    {
-        fDNAPhysicsList->ConstructProcess(); 
-    }
-    if(fChemistryList_option3 != nullptr)  
-    {
-        fChemistryList_option3->ConstructProcess(); 
-    }
 
-    
+    if(fDNAPhysicsList != nullptr)
+    {
+        fDNAPhysicsList->ConstructProcess();
+    }
+    if(fChemistryList_option3 != nullptr)
+    {
+        fChemistryList_option3->ConstructProcess();
+    }
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void PhysicsList::RegisterConstructor(const G4String& name)
 {
-    if(name == fPhysDNAName) 
+    if(name == fPhysDNAName)
     {
-        return; 
+        return;
     }
-    
-    if(verboseLevel > 0) 
+
+    if(verboseLevel > 0)
     {
-        G4cout << "===== Register constructor ==== " << name << G4endl; 
+        G4cout << "===== Register constructor ==== " << name << G4endl;
     }
-    
-    if(name == "G4EmDNAPhysics_option2") 
+
+    if(name == "G4EmDNAPhysics_option7")
     {
-        fDNAPhysicsList.reset(new G4EmDNAPhysics_option2(verboseLevel));
+        fDNAPhysicsList.reset(new G4EmDNAPhysics_option7(verboseLevel));
         fPhysDNAName = name;
-    } 
-    else if(name == "G4EmDNAChemistry_option3") 
+    }
+    else if(name == "G4EmAndDNA")
+    {
+        fDNAPhysicsList.reset(new G4EmAndDNA(verboseLevel));
+        fPhysDNAName = name;
+    }
+    else if(name == "G4EmDNAChemistry_option3")
     {
         fChemistryList_option3.reset(new G4EmDNAChemistry_option3());
         fChemistryList_option3->SetVerboseLevel(verboseLevel);
-    } 
-    else 
+    }
+    else
     {
         G4cout << "PhysicsList::RegisterConstructor: <" << name << ">"
                << " fails - name is not defined"
-               << G4endl;    
+               << G4endl;
     }
 }
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
